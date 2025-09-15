2d70fc190f93d79ebe498c96a9045af734dae4ac
BarbutiDiandru <diandru_barbuti08@yahoo.com>
2025-09-15 09:41:18 +0300

    add telescope plugin and vim options

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,1 +1,4 @@
+print("Welcome to neovim!")
+require("config.mapleader")
 require("config.lazy")
+require("config.options")
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,43 +1,11 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      {
-        "nvim-lua/plenary.nvim"
-      },
-      {
-        "BurntSushi/ripgrep"
-      },
-    },
-
-    config = function()
-      require('telescope').setup {
-        defaults = require('telescope.themes').get_ivy {},
-        pickers = {
-          find_files = {
-            find_command = {
-              "rg",
-              "--files",
-              "--ignore",
-              "--hidden",
-              "--glob=!.git"
-            },
-          },
-          live_grep = {
-            additional_args = {
-              "--hidden",
-              "--glob=!.git",
-            },
-          },
-        }
-      }
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live_grep' })
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
-      vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = 'Telescope colorschemes' })
-    end
-  }
-}
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+     dependencies = { { 'nvim-lua/plenary.nvim' }, { 'BurntSushi/ripgrep' } },
+     config = function()
+	 local builtin = require('telescope.builtin')
+	 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' }) 
+	 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' }) 
+	 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' }) 
+	 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help_tags' }) 
+     end
+    }
